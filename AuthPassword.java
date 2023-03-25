6fea89a5147d4952b7b0ef96d49f6d5b045f9745
Thayna <thaynas.s502@gmail.com>
2023-03-25 15:47:07 -0300

    criacao de senha para o usuario

--- AuthPassword.java
+++ AuthPassword.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class AuthPassword {
     public static void main(String args[]) {
-        String password = "123456";
+        String password;
         String pass;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Crie sua senha: ");
+        password = scanner.nextLine();
+        System.out.println("Senha criada com sucesso!");
 
         System.out.print("Digite a senha: ");
-        Scanner scanner = new Scanner(System.in);
         pass = scanner.nextLine();
 
         Boolean check = pass.equals(password);
